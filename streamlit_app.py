b54d28d415d7177a1e3402434d91936047e7832e
praful-creator9 <digitalhome9861@gmail.com>
2024-12-27 01:00:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert the nsowpark dataframe to a pandas dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
