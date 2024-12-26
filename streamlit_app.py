aa5e49419cb4a1d11722c745037cdbc72c5c7983
praful-creator9 <digitalhome9861@gmail.com>
2024-12-26 23:58:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
-
 cnx = st.connection("snowflake")
 session = cnx.session()
+from snowflake.snowpark.functions import col
+
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
